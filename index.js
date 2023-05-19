03615d7dcc1759543da2e58601bfbed288430d7d
raymcfarlane <122712555+raymcfarlane@users.noreply.github.com>
2023-05-18 21:47:28 -0400

    remove files and update

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+// Import and require mysql2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+const db = mysql.createConnection(
+  {
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'employee_db'
+  },
+  console.log(`Connected to the employee_db database.`)
+);
+
