c6146b7f69741b3167d31d82d5a63ad51a5bb5aa
raymcfarlane <122712555+raymcfarlane@users.noreply.github.com>
2023-05-30 10:40:59 -0400

    update queury file

--- query.js
+++ query.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection(
         password: 'root',
         database: 'employee_db'
     },
-    console.log(`Connected to the cms_db database.`)
+    console.log(`Connected to the employee_db database.`)
 )
 
 async function determineDBQuery(val) {
